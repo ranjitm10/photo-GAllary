5d260630c5f610e83733482dc975a993958637b0
ranjitm10 <64469138+ranjitm10@users.noreply.github.com>
2021-04-23 21:39:11 +0530

    done some changes

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDvluMwZa1xWlrL-bJlmT8e5K09xbHFvjA",
+  authDomain: "photo-gallery-e0e28.firebaseapp.com",
+  projectId: "photo-gallery-e0e28",
+  storageBucket: "photo-gallery-e0e28.appspot.com",
+  messagingSenderId: "138866529512",
+  appId: "1:138866529512:web:91427c72c8fdcd6e15e55d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
