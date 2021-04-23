efda71e899e6d9d18ce6fb5cbd60f6027c62a531
ranjitm10 <64469138+ranjitm10@users.noreply.github.com>
2021-04-23 22:07:56 +0530

    done some changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,6 +5,7 @@ import 'firebase/firestore';
 var firebaseConfig = {
   apiKey: "AIzaSyDvluMwZa1xWlrL-bJlmT8e5K09xbHFvjA",
   authDomain: "photo-gallery-e0e28.firebaseapp.com",
+  databaseURL: "https://pics-gallery-494a6-default-rtdb.firebaseio.com/"
   projectId: "photo-gallery-e0e28",
   storageBucket: "photo-gallery-e0e28.appspot.com",
   messagingSenderId: "138866529512",
